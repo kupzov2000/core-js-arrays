a5e93a0bc34b5e4daab48224eaf2496d7c9c6b27
Evgeny Kuptsov <kupzov200@mail.ru>
2024-02-13 03:19:03 +0400

    feat: add solution for task "getIdentityMatrix"

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -419,8 +419,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, index) =>
+    Array.from({ length: n }, (__, indexCurr) => (indexCurr === index ? 1 : 0))
+  );
 }
 
 /**
