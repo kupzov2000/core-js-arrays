70257bdbbc936891d41ebafc94c8bfff88360851
Evgeny Kuptsov <kupzov200@mail.ru>
2025-11-14 03:09:53 +0400

    feat: solve the generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -364,8 +364,13 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let prev = null;
+  return Array.from({ length: len }, () => {
+    const current = prev === null ? 1 : prev + 2;
+    prev = current;
+    return current;
+  });
 }
 
 /**
